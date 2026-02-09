06e627336323d47f1266f1cf9c161318f773edfe
Eugene Kimathi <eugenekimathi@gmail.com>
2026-02-09 22:37:55 +0300

    Add Travellog logic

--- script.js
+++ script.js
@@ -18,4 +18,29 @@ Place.prototype.getLandmarks = function(){
     return this.landmarks.join(", ");
 };
 
+// Travellog Logic
+function TravelLog () {
+   this.places = {};
+    this.currentId = 0; 
+}
 
+TravelLog.prototype.addPlace = function(place){
+    this.currentId += 1;
+    place.id = this.currentId;
+    this.places[this.currentId] = place;
+};
+
+TravelLog.prototype.findPlace = function(id){
+    if (this.places[id] !== undefined) {
+        return this.places[id];
+    }
+    return undefined;
+};
+
+TravelLog.prototype.deletePlace = function(id){
+    if (this.places[id] !== undefined) {
+        delete this.places[id];
+        return true;
+    }
+    return false;
+}
